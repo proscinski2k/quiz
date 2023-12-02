82f7e939d47296b71cdd52a00bd7f545f33960fb
DESKTOP-77G2KSI\Adrian <proscinski2k@gmail.com>
2023-12-02 15:52:38 +0100

    adriancommit

--- public/index.js
+++ public/index.js
@@ -1,4 +1,8 @@
 import App from './App.js';
 import appData from './data/test-data.js';
 const app = new App();
+app.selectTest.onTestSelected = (selectedTest) => {
+    if (selectedTest)
+        app.startTest(selectedTest);
+};
 app.loadTestData(appData);
