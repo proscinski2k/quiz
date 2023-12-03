5a9b6940c6e72c93703ac2c426506a7741a4b36a
proscinski2k <proscinski2k@gmail.com>
2023-12-03 14:53:49 +0100

    Add authors and initial result summary view

--- public/class/ManageViews.js
+++ public/class/ManageViews.js
@@ -6,7 +6,10 @@ export default class ManageView {
         this.quizStartViewNode = document.querySelector('#quiz-start-view');
         // QUIZ VIEW
         this.quizViewNode = document.querySelector('#quiz-view');
+        // AUTHORS VIEW
+        this.authorsViewNode = document.querySelector('#authors-view');
         this.backToQuizzesViewNodes = document.querySelectorAll('.back-to-quizzes-view');
+        this.btnGoToAuthorsViewNode = document.querySelector('#button-go-to-authors-view');
         this.changeVisibleSelectQuizView = (visible) => {
             const quizzesSelectView = this.quizzesSelectViewNode.classList;
             if (visible)
@@ -28,14 +31,31 @@ export default class ManageView {
             else
                 quizView.add('hidden');
         };
+        this.changeVisibleAuthorsView = (visible) => {
+            const authorsView = this.authorsViewNode.classList;
+            if (visible)
+                authorsView.remove('hidden');
+            else
+                authorsView.add('hidden');
+        };
+        this.hideAllViews = () => {
+            this.changeVisibleAuthorsView(false);
+            this.changeVisibleStartQuizView(false);
+            this.changeVisibleQuizView(false);
+            this.changeVisibleSelectQuizView(false);
+        };
         this.addEventListeners = () => {
             this.backToQuizzesViewNodes.forEach(element => {
                 element.addEventListener('click', () => {
+                    this.hideAllViews();
                     this.changeVisibleSelectQuizView(true);
-                    this.changeVisibleStartQuizView(false);
-                    this.changeVisibleQuizView(false);
                     this.endQuiz();
                 });
+            });
+            this.btnGoToAuthorsViewNode.addEventListener('click', () => {
+                this.hideAllViews();
+                this.changeVisibleAuthorsView(true);
+                this.endQuiz();
             });
         };
         this.addEventListeners();
--- src/class/ManageViews.ts
+++ src/class/ManageViews.ts
@@ -5,7 +5,10 @@ export default class ManageView {
     quizStartViewNode: HTMLDivElement = document.querySelector('#quiz-start-view')!
     // QUIZ VIEW
     quizViewNode: HTMLDivElement = document.querySelector('#quiz-view')!
+    // AUTHORS VIEW
+    authorsViewNode: HTMLDialogElement = document.querySelector('#authors-view')!
     backToQuizzesViewNodes: NodeListOf<Element> = document.querySelectorAll('.back-to-quizzes-view')!
+    btnGoToAuthorsViewNode: HTMLButtonElement = document.querySelector('#button-go-to-authors-view')!
     endQuiz: () => void
 
     constructor (endQuiz: () => void) {
@@ -31,14 +34,31 @@ export default class ManageView {
         else quizView.add('hidden')
     }
 
+    changeVisibleAuthorsView = (visible: boolean): void => {
+        const authorsView = this.authorsViewNode.classList
+        if (visible) authorsView.remove('hidden')
+        else authorsView.add('hidden')
+    }
+
+    hideAllViews = (): void => {
+        this.changeVisibleAuthorsView(false)
+        this.changeVisibleStartQuizView(false)
+        this.changeVisibleQuizView(false)
+        this.changeVisibleSelectQuizView(false)
+    }
+
     addEventListeners = (): void => {
         this.backToQuizzesViewNodes.forEach(element => {
             element.addEventListener('click', () => {
+                this.hideAllViews()
                 this.changeVisibleSelectQuizView(true)
-                this.changeVisibleStartQuizView(false)
-                this.changeVisibleQuizView(false)
                 this.endQuiz()
             })
+        })
+        this.btnGoToAuthorsViewNode.addEventListener('click', () => {
+            this.hideAllViews()
+            this.changeVisibleAuthorsView(true)
+            this.endQuiz()
         })
     }
 }
